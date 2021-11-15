964c2f44af175b2617dd271db27d0ec84a893da5
ugvignon <ugo.vignon@etu.uca.fr>
2021-11-15 20:20:03 +0100

    Début HTML/CSS

---
+++ views/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8"/>
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Site bootstrap</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" media="screen">
+	<link href="../css/style.css" type="text/css" rel="stylesheet" media="screen"/>
+</head>
+
+
+<body>
+<header>
+<!--Navigation-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+		<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">	
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a href="#" class="nav-link active">Accueil</a></li>
+				<li class="nav-item">
+					<a href="#synopsis" class="nav-link">Synopsis</a></li>
+				<li class="nav-item">
+					<a href="#actualite" class="nav-link">Actualité</a></li>
+				<li class="nav-item">
+					<a href="#acteurs" class="nav-link">Acteurs</a></li>
+				<li class="nav-item">
+					<a href="#contacter" class="nav-link">Contacter</a></li>
+			</ul>
+		</div>
+        </div>
+	</nav>
+</header>
+
+<main>
+
+</main>
+</body>
+</html>
