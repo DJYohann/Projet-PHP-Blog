27901e657fa342db4104006e1dbafeccfd819ed7
ugvignon <ugo.vignon@etu.uca.fr>
2021-11-20 18:41:28 +0100

    petit changement

--- views/Connection.php
+++ views/Connection.php
@@ -5,15 +5,13 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" media="screen">
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen"/>
 </head>
 
-<body>
 
-<style>
-    div{
-        margin: 15px;
-    }
-</style>
+<body>
+<?php include('header.php');?>
+
 <h1 class="text-center">Connexion</h1><br><br>
 <form action="#" method="post" class="d-flex flex-column align-items-center">
     <div class="form-group w-25">
