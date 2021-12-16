810fdf70cf2ffda85a3e74ecd5759eb980f9fb9c
ugvignon <ugo.vignon@etu.uca.fr>
2021-12-16 10:18:19 +0100

    database

--- database/blog.sql
+++ database/blog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:8889
--- Généré le : jeu. 02 déc. 2021 à 23:49
--- Version du serveur :  5.7.34
--- Version de PHP : 8.0.8
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 16 déc. 2021 à 09:17
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,10 +24,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `TAdmin`
+-- Structure de la table `tadmin`
 --
 
-CREATE TABLE IF NOT EXISTS `TAdmin` (
+DROP TABLE IF EXISTS `tadmin`;
+CREATE TABLE IF NOT EXISTS `tadmin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(50) NOT NULL,
   `mdp` varchar(20) NOT NULL,
@@ -35,20 +36,21 @@ CREATE TABLE IF NOT EXISTS `TAdmin` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `TAdmin`
+-- Déchargement des données de la table `tadmin`
 --
 
-INSERT INTO `TAdmin` (`id`, `login`, `mdp`) VALUES
+INSERT INTO `tadmin` (`id`, `login`, `mdp`) VALUES
 (1, 'ugvignon', '1234'),
 (2, 'yobreuil', '0000');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `TComments`
+-- Structure de la table `tcomments`
 --
 
-CREATE TABLE IF NOT EXISTS `TComments` (
+DROP TABLE IF EXISTS `tcomments`;
+CREATE TABLE IF NOT EXISTS `tcomments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_news` int(11) NOT NULL,
   `pseudo` varchar(50) NOT NULL,
@@ -60,27 +62,36 @@ CREATE TABLE IF NOT EXISTS `TComments` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `TNews`
+-- Structure de la table `tnews`
 --
 
-CREATE TABLE IF NOT EXISTS `TNews` (
+DROP TABLE IF EXISTS `tnews`;
+CREATE TABLE IF NOT EXISTS `tnews` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date_creation` date NOT NULL,
   `title` varchar(50) NOT NULL,
   `author` varchar(50) NOT NULL,
   `content` longtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `tnews`
+--
+
+INSERT INTO `tnews` (`id`, `date_creation`, `title`, `author`, `content`) VALUES
+(1, '2021-09-22', 'jean', 'moi', 'nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn'),
+(2, '2021-10-05', 'j\'aime la tartiflette mais j\'en mange pas bcp', 'moi', 'gj fftkhb');
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `TComments`
+-- Contraintes pour la table `tcomments`
 --
-ALTER TABLE `TComments`
-  ADD CONSTRAINT `tcomments_ibfk_1` FOREIGN KEY (`id_news`) REFERENCES `TComments` (`id`) ON DELETE CASCADE;
+ALTER TABLE `tcomments`
+  ADD CONSTRAINT `tcomments_ibfk_1` FOREIGN KEY (`id_news`) REFERENCES `tcomments` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
